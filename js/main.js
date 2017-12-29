cf7f18d385726fc61cb42e1f0459dc46cf902189
baiyunpeng <baiyunpeng@yidian-inc.com>
2017-12-29 12:34:03 +0800

    Site updated: 2017-12-29 12:34:03

--- js/main.js
+++ js/main.js
@@ -1,145 +1,382 @@
-var Tips = (function(){
+// var utils = {
+//   norm: function(value, min, max) {
+//     return (value - min) / (max - min);
+//   },
 
-	var $tipBox = $(".tips-box");
+//   lerp: function(norm, min, max) {
+//     return (max - min) * norm + min;
+//   },
 
-	var bind = function(){
+//   map: function(value, sourceMin, sourceMax, destMin, destMax) {
+//     return utils.lerp(utils.norm(value, sourceMin, sourceMax), destMin, destMax);
+//   },
 
-	}
+//   clamp: function(value, min, max) {
+//     return Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max));
+//   },
 
-	bind();
-	return {
-		show: function(){
-			$tipBox.removeClass("hide");
-		},
-		hide: function(){
-			$tipBox.addClass("hide");
-		},
-		init: function(){
-			
-		}
-	}
-})();
+//   distance: function(p0, p1) {
+//     var dx = p1.x - p0.x,
+//       dy = p1.y - p0.y;
+//     return Math.sqrt(dx * dx + dy * dy);
+//   },
 
-var Main = (function(){
+//   distanceXY: function(x0, y0, x1, y1) {
+//     var dx = x1 - x0,
+//       dy = y1 - y0;
+//     return Math.sqrt(dx * dx + dy * dy);
+//   },
 
-	var resetTags = function(){
-		var tags = $(".tagcloud a");
-		tags.css({"font-size": "12px"});
-		for(var i=0,len=tags.length; i<len; i++){
-			//var num = parseInt(Math.random()*5+1);
-			var num = tags.eq(i).html().length % 5 +1;
-			tags[i].className = "";
-			tags.eq(i).addClass("color"+num);
-		}
-	}
+//   circleCollision: function(c0, c1) {
+//     return utils.distance(c0, c1) <= c0.radius + c1.radius;
+//   },
 
-	var slide = function(idx){
-		var $wrap = $(".switch-wrap");
-		$wrap.css({
-			"transform": "translate(-"+idx*100+"%, 0 )"
-		});
-		$(".icon-wrap").addClass("hide");
-		$(".icon-wrap").eq(idx).removeClass("hide");
-	}
+//   circlePointCollision: function(x, y, circle) {
+//     return utils.distanceXY(x, y, circle.x, circle.y) < circle.radius;
+//   },
 
-	var bind = function(){
-		var switchBtn = $("#myonoffswitch");
-		var tagcloud = $(".second-part");
-		var navDiv = $(".first-part");
-		switchBtn.click(function(){
-			if(switchBtn.hasClass("clicked")){
-				switchBtn.removeClass("clicked");
-				tagcloud.removeClass("turn-left");
-				navDiv.removeClass("turn-left");
-			}else{
-				switchBtn.addClass("clicked");
-				tagcloud.addClass("turn-left");
-				navDiv.addClass("turn-left");
-				resetTags();
-			}
-		});
+//   pointInRect: function(x, y, rect) {
+//     return utils.inRange(x, rect.x, rect.x + rect.radius) &&
+//       utils.inRange(y, rect.y, rect.y + rect.radius);
+//   },
 
-		var timeout;
-		var isEnterBtn = false;
-		var isEnterTips = false;
+//   inRange: function(value, min, max) {
+//     return value >= Math.min(min, max) && value <= Math.max(min, max);
+//   },
 
-		$(".icon").bind("mouseenter", function(){
-			isEnterBtn = true;
-			Tips.show();
-		}).bind("mouseleave", function(){
-			isEnterBtn = false;
-			setTimeout(function(){
-				if(!isEnterTips){
-					Tips.hide();
-				}
-			}, 100);
-		});
+//   rangeIntersect: function(min0, max0, min1, max1) {
+//     return Math.max(min0, max0) >= Math.min(min1, max1) &&
+//       Math.min(min0, max0) <= Math.max(min1, max1);
+//   },
 
-		$(".tips-box").bind("mouseenter", function(){
-			isEnterTips = true;
-			Tips.show();
-		}).bind("mouseleave", function(){
-			isEnterTips = false;
-			setTimeout(function(){
-				if(!isEnterBtn){
-					Tips.hide();
-				}
-			}, 100);
-		});
+//   rectIntersect: function(r0, r1) {
+//     return utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
+//       utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
+//   },
 
-		$(".tips-inner li").bind("click", function(){
-			var idx = $(this).index();
-			slide(idx);
-			Tips.hide();
-		});
-	}
+//   degreesToRads: function(degrees) {
+//     return degrees / 180 * Math.PI;
+//   },
 
-	var fancyInit = function(){
-		var isFancy = $(".isFancy");
-		if(isFancy.length != 0){
-			var imgArr = $(".article-inner img");
-			for(var i=0,len=imgArr.length;i<len;i++){
-				var src = imgArr.eq(i).attr("src");
-				var title = imgArr.eq(i).attr("alt");
-				imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
-			}
-			$(".article-inner .fancy-ctn").fancybox();
-		}
-	}
+//   radsToDegrees: function(radians) {
+//     return radians * 180 / Math.PI;
+//   },
 
-	var enterAnm = function(){
-		//avatar
-		$(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
-		$(".js-avatar")[0].onload = function(){
-			$(".js-avatar").addClass("show");
-		}
+//   randomRange: function(min, max) {
+//     return min + Math.random() * (max - min);
+//   },
 
-		//article
-		function showArticle(){
-			$(".article").each(function(){
-				if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && !$(this).hasClass('show') ) {
-					$(this).addClass("show");
-				}
-			});
-		}
-		$(window).on('scroll', function(){
-			showArticle();
-		});
-		showArticle();
-	}
+//   randomInt: function(min, max) {
+//     return min + Math.random() * (max - min + 1);
+//   },
 
-	return {
-		init: function(){
-			resetTags();
-			bind();
-			enterAnm();
-			fancyInit();
-			Tips.init();
-			new Mobile({
-				ctn: document.getElementsByClassName("slider-trigger")[0]
-			});
-		}
-	}
-})();
+//   getmiddle: function(p0, p1) {
+//     var x = p0.x,
+//       x2 = p1.x;
+//     middlex = (x + x2) / 2;
+//     var y = p0.y,
+//       y2 = p1.y;
+//     middley = (y + y2) / 2;
+//     pos = [middlex, middley];
 
-$(Main.init());
+//     return pos;
+//   },
+
+//   getAngle: function(p0, p1) {
+//     var deltaX = p1.x - p0.x;
+//     var deltaY = p1.y - p0.y;
+//     var rad = Math.atan2(deltaY, deltaX);
+//     return rad;
+//   },
+//   inpercentW: function(size) {
+//     return (size * W) / 100;
+//   },
+
+//   inpercentH: function(size) {
+//     return (size * H) / 100;
+//   },
+
+// }
+
+// // 声明  全局变量
+// var slider = document.getElementById("slider");
+// var canvas, ctx, W, H, gridX, gridY, gravity, duration, resolution, speed, radius;
+// var timer;
+
+
+// function shape(x, y, texte) {
+//   this.x = x;
+//   this.y = y;
+//   this.size = W/6;
+
+//   this.text = texte;
+//   this.placement = [];
+//   this.vectors = [];
+
+// }
+
+// shape.prototype.getValue = function() {
+//   console.log("get black pixels position");
+
+//   // Draw the shape :^)
+
+//   ctx.textAlign = "center";
+//   ctx.font = "bold " + this.size + "px arial";
+//   ctx.fillText(this.text, this.x, this.y);
+
+
+//   var idata = ctx.getImageData(0, 0, W, H);
+
+//   var buffer32 = new Uint32Array(idata.data.buffer);
+
+//   for (var y = 0; y < H; y += gridY) {
+//     for (var x = 0; x < W; x += gridX) {
+
+//       if (buffer32[y * W + x]) {
+//         this.placement.push(new particle(x, y));
+//       }
+//     }
+//   }
+//   ctx.clearRect(0, 0, W, H);
+
+// }
+
+// var colors = [
+//   '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+//   '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
+//   '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+//   '#FF5722'
+// ];
+
+// function particle(x, y, type) {
+//   this.radius = 1.1;
+//   this.futurRadius = utils.randomInt(radius, radius+3);
+  
+  
+//   this.rebond = utils.randomInt(1, 5);
+//   this.x = x;
+//   this.y = y;
+  
+//   this.dying = false;
+  
+//   this.base = [x, y]
+
+//   this.vx = 0;
+//   this.vy = 0;
+//   this.type = type;
+//   this.friction = .99;
+//   this.gravity = gravity;
+//   this.color = colors[Math.floor(Math.random() * colors.length)];
+
+//   this.getSpeed = function() {
+//     return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+//   };
+
+//   this.setSpeed = function(speed) {
+//     var heading = this.getHeading();
+//     this.vx = Math.cos(heading) * speed;
+//     this.vy = Math.sin(heading) * speed;
+//   };
+
+//   this.getHeading = function() {
+//     return Math.atan2(this.vy, this.vx);
+//   };
+
+//   this.setHeading = function(heading) {
+//     var speed = this.getSpeed();
+//     this.vx = Math.cos(heading) * speed;
+//     this.vy = Math.sin(heading) * speed;
+//   };
+
+//   this.angleTo = function(p2) {
+//     return Math.atan2(p2.y - this.y, p2.x - this.x);
+
+//   };
+
+//   this.update = function(heading) {
+//     this.x += this.vx;
+//     this.y += this.vy;
+//     this.vy += gravity;
+
+//     this.vx *= this.friction;
+//     this.vy *= this.friction;
+    
+//     if(this.radius < this.futurRadius && this.dying === false){
+//       this.radius += duration;
+//     }else{
+//       this.dying = true;
+//     }
+      
+//     if(this.dying === true){
+//       this.radius -= duration;
+
+      
+//     }
+ 
+
+//     ctx.beginPath();
+
+//     ctx.fillStyle = this.color;
+
+//     ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+//     ctx.fill();
+//     ctx.closePath();
+
+//     if (this.y < 0 || this.radius < 1) {
+//       this.x = this.base[0];
+//       this.dying = false;
+//       this.y = this.base[1];
+//       this.radius = 1.1;
+//       this.setSpeed(speed);
+//       this.futurRadius = utils.randomInt(radius, radius+3);
+//       this.setHeading(utils.randomInt(utils.degreesToRads(0), utils.degreesToRads(360)));
+//     }
+
+//   };
+
+//   this.setSpeed(utils.randomInt(.1, .5));
+//   this.setHeading(utils.randomInt(utils.degreesToRads(0), utils.degreesToRads(360)));
+
+// }
+
+// if (slider) {
+//   function init () {
+//     canvas = document.getElementById("canvas");
+//     ctx = canvas.getContext('2d');
+//     W = canvas.width = slider.offsetWidth;
+//     H = canvas.height = slider.offsetHeight;
+
+//     gridX = 5;
+//     gridY = 5;
+
+//     gravity = parseFloat(document.getElementById("2").value);
+//     duration =  parseFloat(document.getElementById("3").value);
+//     resolution = parseFloat(document.getElementById("4").value);
+//     speed = parseFloat(document.getElementById("5").value);
+//     radius = parseFloat(document.getElementById("6").value);
+
+//     fieldvalue = document.getElementById("message");
+
+//     message = new shape(W / 2, H / 2 + 50, fieldvalue.value);
+
+//     message.getValue();
+
+//     update();  
+//   }
+//   init()
+// }
+
+
+// function change() {
+//   ctx.clearRect(0, 0, W, H);
+
+//   gridX = parseFloat(element4.value);
+//   gridY = parseFloat(element4.value);
+//   message.placement = [];
+//   message.text = fieldvalue.value;
+//   message.getValue();
+// }
+
+
+// function changeV() {
+//     gravity = parseFloat(element2.value);
+//     duration =  parseFloat(element3.value);
+//     speed = parseFloat(element5.value);
+//     radius = parseFloat(element6.value);
+// }
+
+// var fps = 100;
+// function update() {
+//   clearTimeout(timer);
+//   timer = setTimeout(function() {
+//     ctx.clearRect(0, 0, W, H);
+
+
+//     for (var i = 0; i < message.placement.length; i++) {
+//       message.placement[i].update();
+//     }
+
+//     requestAnimationFrame(update);
+//   }, 1000 / fps);
+// }
+
+// window.onresize = function () {
+//   init && init();
+// }
+
+var timer = null;
+
+var appendArrow = function() {
+  var slider = document.querySelector('.slider');
+  var magic = document.querySelector('.magic-link');
+  var arrow = document.createElement('div');
+  arrow.innerHTML = `
+    <div class='arrow'>
+      <img src='/images/home/arrow.svg'/>
+    <div>
+  `;
+  if (slider) {
+    slider.appendChild(arrow);
+    arrow.addEventListener('click', function() {
+      var target = slider.offsetHeight + magic.offsetHeight;
+      myScrollTo(target, 10, function() {
+        slider.style.display = 'none';
+        magic.style.display = 'none';
+        document.body.scrollTop = 0;
+      })
+    })
+  }
+}
+var myScrollTo = function(target, speed, fn){
+  timer && clearTimeout(timer);
+  var _top = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop || 0;
+  if (_top < target) {
+    _top += speed;
+    window.scrollTo(0,_top);
+  } else {
+    fn && fn()
+    return ;
+  }
+  timer = setTimeout(function(){
+    myScrollTo(target, speed, fn);
+  }, 1)
+}
+
+var navbarBurger = document.querySelector('#navbarBurger');
+navbarBurger.addEventListener('click', function (el) {
+  document.querySelector('#navMenuIndex').classList.toggle("is-active"); 
+});
+
+//资源预加载
+if (is_home) {
+  var loader = new resLoader({
+    resources : [
+     '/images/home/bg.jpg',
+     '/images/home/home.png',
+     '/images/home/home_hover.png',
+     '/images/home/Blog.png',
+     '/images/home/Blog_hover.png',
+     '/images/home/about.png',
+     '/images/home/about_hover.png',
+     '/images/home/nav.png',
+     '/images/home/nav_hover.png',
+     '/images/home/source.png',
+     '/images/home/source_hover.png',
+     '/images/home/game.png',
+     '/images/home/game_hover.png'
+    ],
+    onStart : function(total){
+      // console.log('start:'+total);
+    },
+    onProgress : function(current, total){
+      // console.log(current+'/'+total);
+      // var percent = current/total*100;
+      // console.log(percent+ '%')
+    },
+    onComplete : function(total){
+      appendArrow();
+      document.querySelector('.mask').classList.toggle("mask-hide"); 
+    }
+  });
+  loader.start();  
+}
